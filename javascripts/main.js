b2e28d2073a74a022f4a10929b4a7d12988bba12
Harrison Chen <harrisonxchen@gmail.com>
2014-10-11 18:34:04 -0700

    dropdown

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,13 @@ app.directive('mainNav', function(){
 									'<li><a href="about.html">About</span></a></li>' +
 									'<li><a href="">Blog</a></li></div>' +
 									'<li><a href="assets/hc-resume-2014.pdf">Portfolio</a></li></div>' +
-									'<li><a href="https://github.com/HarrisonChen">Explore&darr;</a></li>' +
+									'<li class="dropdown-btn">' +
+										'<a href="https://github.com/HarrisonChen">Explore&darr;</a>' +
+										'<ul>' +
+											'<li><a href="">Photography</a></li>' +
+											'<li><a href="">Videos</a></li>' +
+										'</ul>' +
+									'</li>' +
 								'</ul>' +
 							'</nav>',
 		controller: function($scope, $element){
