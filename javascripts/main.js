f79d03af948509fe46be761070f32c69412d56c9
Harrison Chen <harrisonxchen@gmail.com>
2014-10-11 23:29:52 -0700

    parallax scrolling

--- javascripts/main.js
+++ javascripts/main.js
@@ -26,3 +26,67 @@ app.directive('mainNav', function(){
 		}
 	}
 });
+
+app.directive('vpParallax', function($window){
+	return{
+		restrict: 'AE',
+		template: 
+			'<a href="#">' +
+				'<div class="viewport-img" id="vp-adventure">' +
+					'<img src="images/gospel-rock-panorama.jpg" alt="Gospel Rock Panorama" />' +
+					'<div class="ctr-img-txt adventure-txt">' +
+						'#adventure' +
+					'</div>' +
+			  '</div>' +
+			'</a>' +
+			'<a href="#">' +
+			  '<div class="viewport-img" id="vp-photography">' +
+					'<img src="images/golden-gate-bridge.jpg" alt="Golden Gate Bridge" />' +
+					'<div class="ctr-img-txt photography-txt">' +
+						'#photography' +
+					'</div>' +
+			  '</div>' +
+			'</a>' +
+			'<a href="#">' +
+				'<div class="viewport-img" id="vp-bike">' +
+					'<img src="images/mtb-1.jpg" alt="Mountain Biking" />' +
+					'<div class="ctr-img-txt bike-txt">' +
+						'#bike' +
+					'</div>' +
+			  '</div>' +
+			'</a>',
+		controller: function($scope, $element){
+			$scope.parallaxAdventure = function(){
+				if($scope.scrollPos + window.innerHeight >= $scope.vpBikePos){
+					$scope.vpBike.addClass("viewport-img-expand");
+				}
+				if($scope.scrollPos + window.innerHeight >= $scope.vpPhotographyPos){
+					$scope.vpPhotography.addClass("viewport-img-expand");
+				}
+				if($scope.scrollPos + window.innerHeight >= $scope.vpAdventurePos){
+					$scope.vpAdventure.addClass("viewport-img-expand");
+				}
+				$scope.vpAdventure = angular.element(document.querySelector('#vp-adventure'));
+				$scope.vpPhotography = angular.element(document.querySelector('#vp-photography'));
+				$scope.vpBike = angular.element(document.querySelector('#vp-bike'));
+				$scope.vpAdventurePos = $scope.vpAdventure[0].offsetTop;
+				$scope.vpPhotographyPos = $scope.vpPhotography[0].offsetTop;
+				$scope.vpBikePos = $scope.vpBike[0].offsetTop;
+			}
+
+			angular.element($window).bind("scroll", function(e){
+				$scope.scrollPos = window.top.scrollY;
+				$scope.parallaxAdventure();
+			});
+		},
+		link: function(scope, element, attrs){
+			scope.scrollPos = window.top.scrollY;
+			scope.vpAdventure = angular.element(document.querySelector('#vp-adventure'));
+			scope.vpPhotography = angular.element(document.querySelector('#vp-photography'));
+			scope.vpBike = angular.element(document.querySelector('#vp-bike'));
+			scope.vpAdventurePos = scope.vpAdventure[0].offsetTop;
+			scope.vpPhotographyPos = scope.vpPhotography[0].offsetTop;
+			scope.vpBikePos = scope.vpBike[0].offsetTop;
+		}
+	}
+});
