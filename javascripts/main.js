c992f8aebbc4830ce3177661eb7db0b0618a1875
Harrison Chen <harrisonxchen@gmail.com>
2014-10-12 00:57:03 -0700

    efficient transitions

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,13 +7,15 @@ app.directive('mainNav', function(){
 								'<a href="index.html"><div class="home-btn" /></div></a>' +
 								'<ul>' +
 									'<li><a href="about.html">About</span></a></li>' +
-									'<li><a href="">Blog</a></li></div>' +
-									'<li><a href="assets/hc-resume-2014.pdf">Portfolio</a></li></div>' +
+									'<li><a href="assets/hc-resume-2014.pdf">Blog</a></li></div>' +
+									'<li><a href="portfolio.html">Portfolio</a></li></div>' +
 									'<li class="dropdown-btn">' +
-										'<a href="https://github.com/HarrisonChen">Explore&darr;</a>' +
+										'<a href="">Explore&darr;</a>' +
 										'<ul>' +
 											'<li><a href="">Photography</a></li>' +
 											'<li><a href="">Videos</a></li>' +
+											'<li><a href="https://www.linkedin.com/in/harrisonwchen">LinkedIn</li>' +
+											'<li><a href="https://github.com/HarrisonChen">GitHub</li>' +
 										'</ul>' +
 									'</li>' +
 								'</ul>' +
@@ -56,37 +58,48 @@ app.directive('vpParallax', function($window){
 			  '</div>' +
 			'</a>',
 		controller: function($scope, $element){
-			$scope.parallaxAdventure = function(){
+			$scope.scrollPos = window.top.scrollY;
+
+			$scope.vpAdventure = angular.element(document.querySelector('#vp-adventure'));
+			$scope.vpPhotography = angular.element(document.querySelector('#vp-photography'));
+			$scope.vpBike = angular.element(document.querySelector('#vp-bike'));
+			$scope.vpAdventurePos = $scope.vpAdventure[0].offsetTop;
+			$scope.vpPhotographyPos = $scope.vpPhotography[0].offsetTop;
+			$scope.vpBikePos = $scope.vpBike[0].offsetTop;
+
+			var finishScroll = false;
+
+			$scope.vpParallax = function(){
 				if($scope.scrollPos + window.innerHeight >= $scope.vpBikePos){
-					$scope.vpBike.addClass("viewport-img-expand");
-				}
-				if($scope.scrollPos + window.innerHeight >= $scope.vpPhotographyPos){
-					$scope.vpPhotography.addClass("viewport-img-expand");
-				}
-				if($scope.scrollPos + window.innerHeight >= $scope.vpAdventurePos){
 					$scope.vpAdventure.addClass("viewport-img-expand");
+					$scope.vpPhotography.addClass("viewport-img-expand");
+					$scope.vpBike.addClass("viewport-img-expand");
+					finishScroll = true;
 				}
-				$scope.vpAdventure = angular.element(document.querySelector('#vp-adventure'));
-				$scope.vpPhotography = angular.element(document.querySelector('#vp-photography'));
-				$scope.vpBike = angular.element(document.querySelector('#vp-bike'));
-				$scope.vpAdventurePos = $scope.vpAdventure[0].offsetTop;
-				$scope.vpPhotographyPos = $scope.vpPhotography[0].offsetTop;
-				$scope.vpBikePos = $scope.vpBike[0].offsetTop;
+				else if($scope.scrollPos + window.innerHeight >= $scope.vpPhotographyPos){
+					$scope.vpPhotography.addClass("viewport-img-expand");
+					$scope.vpBike = angular.element(document.querySelector('#vp-bike'));
+					$scope.vpBikePos = $scope.vpBike[0].offsetTop;
+				}
+				else if($scope.scrollPos + window.innerHeight >= $scope.vpAdventurePos){
+					$scope.vpAdventure.addClass("viewport-img-expand");
+					$scope.vpPhotography = angular.element(document.querySelector('#vp-photography'));
+					$scope.vpPhotographyPos = $scope.vpPhotography[0].offsetTop;
+				}
 			}
 
+			$scope.vpParallax();
+
 			angular.element($window).bind("scroll", function(e){
-				$scope.scrollPos = window.top.scrollY;
-				$scope.parallaxAdventure();
+				if(!finishScroll){
+					console.log($scope.scrollPos);
+					$scope.scrollPos = window.top.scrollY;
+					$scope.vpParallax();
+				}
 			});
 		},
 		link: function(scope, element, attrs){
-			scope.scrollPos = window.top.scrollY;
-			scope.vpAdventure = angular.element(document.querySelector('#vp-adventure'));
-			scope.vpPhotography = angular.element(document.querySelector('#vp-photography'));
-			scope.vpBike = angular.element(document.querySelector('#vp-bike'));
-			scope.vpAdventurePos = scope.vpAdventure[0].offsetTop;
-			scope.vpPhotographyPos = scope.vpPhotography[0].offsetTop;
-			scope.vpBikePos = scope.vpBike[0].offsetTop;
+			
 		}
 	}
 });
