765c068d7f080fea4a0a9b15a42b6c3a8c36e086
Harrison Chen <harrisonxchen@gmail.com>
2014-11-27 12:00:45 -0800

    redo webpage

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,37 +1,83 @@
-var app = angular.module('iris', []);
+var app = angular.module('app', []);
 
-app.controller('MainCtrl', ['$scope', '$window', function($scope, $window){
+app.controller('tabsCtrl', ['$scope', function($scope){
+	$scope.about = function(){
+		$scope.variable = "container-right";
+	};
+}]);
 
-	var finishScroll = false;
+app.controller('terminalCtrl', ['$scope', '$timeout', function($scope, $timeout){
+	// var message = ['H', 'e', 'l', 'l', 'o'];
+	var message1 = "Hello World,";
+	var message2 = " my name is Harrison!";
+	var message3 = "I study computer engineering at UC Riverside";
+	var message4 = "I love web & mobile application";
+	var message5 = "I love web & mobile app";
+	var message6 = " development";
+	var message7 = "Sometimes I workout, mountain bike, and go rockclimbing";
+	var message8 = "Enough about me,";
+	var message9 = " take a look around my page!";
+	$scope.message = "";
+	waitTime = 0;
 
-	var titles  = document.getElementsByClassName("title");
-	var parallax_2 = titles[1];
-	var parallax_10 = titles[2];
-	$scope.class_2 = false;
-	$scope.class_10 = false;
-	$scope.class_android = false;
-	var parallax_2_pos = titles[0].offsetTop + titles[0].clientHeight +
-						  titles[1].offsetTop + titles[1].clientHeight;
-  var parallax_10_pos = parallax_2_pos + titles[2].offsetTop + titles[2].clientHeight;
+	pause(3000);
+	showMessage(message1);
+	pause(1000);
+	showMessage(message2);
+	pause(1500);
+	deleteMessage(message1 + message2);
+	showMessage(message3);
+	pause(1500);
+	deleteMessage(message3);
+	showMessage(message4);
+	pause(300);
+	editMessage(message4, message5);
+	showMessage(message6);
+	pause(1500);
+	deleteMessage(message5 + message6);
+	showMessage(message7);
+	pause(1500);
+	deleteMessage(message7);
+	showMessage(message8);
+	pause(1000);
+	showMessage(message9);
 
-	$scope.checkPositions = function(){
-		if(($scope.scrollPos + $window.innerHeight) >= (parallax_2_pos - 100)){
-			$scope.class_2 = true;
-			$scope.class_android = true;
-			$scope.$apply();
-		}
-		if(($scope.scrollPos + $window.innerHeight) >= (parallax_10_pos + 100)){
-			$scope.class_10 = true;
-			$scope.$apply();
-			finishScroll = true;
+	function timerHelper(){
+		waitTime += 50;
+		return waitTime;
+	}
+
+	function setMessage(message, i){
+		$timeout(function(){
+			$scope.message += message[i];
+		}, timerHelper());
+	}
+
+	function setMessageReverse(message, i){
+		$timeout(function(){
+			$scope.message = message.substring(0, i);
+		}, timerHelper());
+	}
+
+	function showMessage(message){
+		for(var i = 0; i < message.length; i++){
+			setMessage(message, i);
 		}
 	}
 
-	angular.element($window).bind("scroll", function(e){
-		if(!finishScroll){
-			$scope.scrollPos = window.top.scrollY;
-			$scope.checkPositions();
-			console.log($scope.scrollPos + $window.innerHeight);
+	function deleteMessage(message){
+		for(var i = message.length; i >= 0; i--){
+			setMessageReverse(message, i);
 		}
-	});
+	}
+
+	function editMessage(message, newMessage){
+		for(var i = message.length; i >= newMessage.length; i--){
+			setMessageReverse(message, i);
+		}
+	}
+
+	function pause(milliseconds){
+		waitTime += milliseconds;
+	}
 }]);
