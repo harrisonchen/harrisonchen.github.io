6d5e3828a771ab311e9b740e05e16c3e4fc773c2
Harrison Chen <harrisonxchen@gmail.com>
2015-02-12 16:15:45 -0800

    v2 basic

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,78 +6,86 @@ app.controller('tabsCtrl', ['$scope', function($scope){
 	};
 }]);
 
-app.controller('terminalCtrl', ['$scope', '$timeout', function($scope, $timeout){
-	// var message = ['H', 'e', 'l', 'l', 'o'];
-	var message1 = "Hello World,";
-	var message2 = " my name is Harrison!";
-	var message3 = "I study computer engineering at UC Riverside";
-	var message4 = "I love web & mobile application";
-	var message5 = "I love web & mobile app";
-	var message6 = " development";
-	var message7 = "Sometimes I workout, mountain bike, and go rockclimbing";
-	var message8 = "Enough about me,";
-	var message9 = " take a look around my page!";
-	$scope.message = "";
-	waitTime = 0;
+app.directive('terminalCtrl', ['$timeout', function($timeout) {
+	return {
+		restrict: 'AE',
+		scope: {},
+		template: '&lt <span ng-bind="message"></span> &gt',
+		controller: function($scope, $element) {
 
-	pause(3000);
-	showMessage(message1);
-	pause(1000);
-	showMessage(message2);
-	pause(1500);
-	deleteMessage(message1 + message2);
-	showMessage(message3);
-	pause(1500);
-	deleteMessage(message3);
-	showMessage(message4);
-	pause(300);
-	editMessage(message4, message5);
-	showMessage(message6);
-	pause(1500);
-	deleteMessage(message5 + message6);
-	showMessage(message7);
-	pause(1500);
-	deleteMessage(message7);
-	showMessage(message8);
-	pause(1000);
-	showMessage(message9);
+		},
+		link: function(scope, element, attrs) {
+			var message1 = "Hello World!";
+			var message3 = "I am a software engineer studying at UC Riverside.";
+			var message4 = "I love web & mobile application";
+			var message5 = "I love web & mobile app";
+			var message6 = " development!";
+			var message7 = "Sometimes I workout, mountain bike, and go rockclimbing";
+			var finalMessage = "Student, Designer, Engineer"
+			scope.message = "";
+			waitTime = 0;
 
-	function timerHelper(){
-		waitTime += 50;
-		return waitTime;
-	}
+			// pause(3000);
+			showMessage(message1);
+			pause(500);
+			deleteMessage(message1);
+			showMessage(message3);
+			pause(750);
+			deleteMessage(message3);
+			showMessage(message4);
+			pause(200);
+			editMessage(message4, message5);
+			showMessage(message6);
+			pause(750);
+			deleteMessage(message5 + message6);
+			showMessage(message7);
+			pause(750);
+			deleteMessage(message7);
+			showMessage(finalMessage);
 
-	function setMessage(message, i){
-		$timeout(function(){
-			$scope.message += message[i];
-		}, timerHelper());
-	}
+			function timerHelper(option){
+				if (option == "write") {
+					waitTime += 40;
+				}
+				else if (option == "delete") {
+					waitTime += 10;
+				}
+				return waitTime;
+			}
 
-	function setMessageReverse(message, i){
-		$timeout(function(){
-			$scope.message = message.substring(0, i);
-		}, timerHelper());
-	}
+			function setMessage(message, i){
+				$timeout(function(){
+					scope.message += message[i];
+				}, timerHelper("write"));
+			}
 
-	function showMessage(message){
-		for(var i = 0; i < message.length; i++){
-			setMessage(message, i);
+			function setMessageReverse(message, i){
+				$timeout(function(){
+					scope.message = message.substring(0, i);
+				}, timerHelper("delete"));
+			}
+
+			function showMessage(message){
+				for(var i = 0; i < message.length; i++){
+					setMessage(message, i);
+				}
+			}
+
+			function deleteMessage(message){
+				for(var i = message.length; i >= 0; i--){
+					setMessageReverse(message, i);
+				}
+			}
+
+			function editMessage(message, newMessage){
+				for(var i = message.length; i >= newMessage.length; i--){
+					setMessageReverse(message, i);
+				}
+			}
+
+			function pause(milliseconds){
+				waitTime += milliseconds;
+			}
 		}
-	}
-
-	function deleteMessage(message){
-		for(var i = message.length; i >= 0; i--){
-			setMessageReverse(message, i);
-		}
-	}
-
-	function editMessage(message, newMessage){
-		for(var i = message.length; i >= newMessage.length; i--){
-			setMessageReverse(message, i);
-		}
-	}
-
-	function pause(milliseconds){
-		waitTime += milliseconds;
 	}
 }]);
